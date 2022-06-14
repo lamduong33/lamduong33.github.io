709ef714b254c026b60dcd1e7b2b0065c127fff9
Lam Duong <lduong@jjrsolutions.com>
2022-06-14 15:14:05 -0400

    feat: New React page.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-//import './App.css';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from "./components/NavBar"
+import NavBar from './components/NavBar';
 
-const lam = "Lam Duong";
+const lam = 'Lam Duong';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <NavBar />
-        <p>
-          Welcome to {lam}'s homepage!
-        </p>
+      <NavBar />
+      <header className="Welcome-page">
+        <p>Welcome to {lam}'s homepage!</p>
+      </header>
+      <header className="Welcome-content">
+        This is a pargaraph.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum."
       </header>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,19 @@
-import { Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+import { Navbar, Container, Nav} from 'react-bootstrap';
 
 export default function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+    <>
+      <Navbar bg="dark" variant="dark" fixed="top" sticky="top">
+        <Container>
+          <Navbar.Brand href="home">Lam Duong</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="home">About Me</Nav.Link>
+            <Nav.Link href="blog">Blog</Nav.Link>
+            <Nav.Link href="videos">Videos</Nav.Link>
+            <Nav.Link href="resume">Resume</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
+    </>
   );
 }
