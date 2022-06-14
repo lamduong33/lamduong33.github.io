f382ab9f2e6602eb6eac7a8f075e196b41b33d76
Lam Duong <lduong@jjrsolutions.com>
2022-06-14 14:10:19 -0400

    fix: Migrating to react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/NavBar"
+
+const lam = "Lam Duong";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to {lam}'s homepage!
         </p>
       </header>
     </div>
