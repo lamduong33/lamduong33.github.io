5544e1907f494375aa6ed37e4e68a3b2c263519a
Lam T Duong <lamthachduong@gmail.com>
2022-06-15 18:12:14 -0400

    fix: Move files to src folder.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import portrait from '../images/Portrait.png'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <header className="Welcome-page">
-        <img src="./"/>
+        <img src={portrait}/>
       </header>
       <header className="Welcome-content">
       </header>
