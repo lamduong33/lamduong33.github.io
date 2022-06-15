2f28775a9b466f241e4ee19853322c613e526bc4
Lam T Duong <lamthachduong@gmail.com>
2022-06-14 22:50:25 -0400

    fix: Removed emoji-dictionary and changed homepage

--- src/App.js
+++ src/App.js
@@ -2,25 +2,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 
-const lam = 'Lam Duong';
-
 function App() {
   return (
     <div className="App">
       <NavBar />
       <header className="Welcome-page">
-        <p>Welcome to {lam}'s homepage!</p>
+        <p> This site is currently under construction. 👷 </p>
+        <p> Please come back later for some fun development! </p>
       </header>
       <header className="Welcome-content">
-        This is a pargaraph.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat.
-      Duis aute irure dolor in reprehenderit
-        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-        officia deserunt mollit anim id est laborum."
       </header>
     </div>
   );
