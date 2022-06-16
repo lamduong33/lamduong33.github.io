738a10d60ae11e2e990257a5e1f17bb109fd0ff0
Lam Duong <lduong@jjrsolutions.com>
2022-06-16 17:13:49 -0400

    feat: Formatting pages.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
-import Portrait from "./images/Portrait.png";
+import Portrait from './images/Portrait.png';
 
 function App() {
   return (
@@ -10,8 +10,13 @@ function App() {
       <header className="Home-page">
         <img className="Portrait" src={Portrait} alt="Lam Duong's portrait" />
         <header className="Home-content">
-          Hi, my name is Lam and I'm a full-stack developer. On my website, you
-          can find some fun and interesting things I work on.
+          <p>
+            My name is Lam Duong and this is my personal website. My interests
+            include computer science, artificial intelligence, neuroscience, and
+            aviation. I write and make videos about these topics from time to
+            time.
+          </p>
+          Github - LinkedIn - Insta?
         </header>
       </header>
     </div>
