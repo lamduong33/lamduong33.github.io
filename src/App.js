ec59cf8cfa7cf25dee258d2efb5b244ea6903cc3
Lam T Duong <lamthachduong@gmail.com>
2022-06-16 00:10:59 -0400

    feat: Started work on homepage.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
-import portrait from './images/Portrait.png'
+import Portrait from "./images/Portrait.png";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <header className="Home-page">
-        <img className = "Portrait" src={portrait}/>
-      </header>
-      <header className="Home-content">
+        <img className="Portrait" src={Portrait} alt="Lam Duong's portrait" />
+        <header className="Home-content">
+          Hi, my name is Lam! I'm
+        </header>
       </header>
     </div>
   );
