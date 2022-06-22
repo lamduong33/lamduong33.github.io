71ad40c23e21679019b3d69ce8e92312731b5f8a
Lam Duong <lduong@jjrsolutions.com>
2022-06-22 17:31:23 -0400

    feat: Styling and new social links

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ function App() {
     <div className="App">
       <NavBar />
       <header className="Home-page">
-        <img className="Portrait" src={Portrait} alt="Lam Duong's portrait" />
         <header className="Home-content">
+          <img className="Portrait" src={Portrait} alt="Lam Duong's portrait" />
+          <p></p>
           <p>
             My name is Lam Duong and this is my personal website. My interests
             include computer science, artificial intelligence, neuroscience, and
             aviation. I write and make videos about these topics from time to
             time.
           </p>
-          Github - LinkedIn - Insta?
+          Social: <a style={{color:'white'}} href="https://github.com/lamduong33"> GitHub</a> -
+          <a style={{color:'white'}} href="https://github.com/lamduong33"> LinkedIn</a>
         </header>
       </header>
     </div>
